2b056d0958e91bac99b5fb194487ed5c49894cfa
szl <463515279@qq.com>
2018-07-20 18:03:12 +0800

    配置环境

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <img src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <input type="text" v-model="msg">
+        <div>{{msg}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue, Prop} from "vue-property-decorator";
+    import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+    @Component({
+        components: {
+            HelloWorld
+        }
+    })
+    export default class Home extends Vue {
+        msg: string = "abc";
+        @Prop
+        
+    }
+</script>
